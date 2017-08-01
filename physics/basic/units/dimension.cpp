a264c3ae4f014e488451095d9263992246db66a9
Ismael Correa <ismael.correa.castro@gmail.com>
2017-08-01 18:15:50 -0400

    Bug of units with the da prefix solved

--- physics/basic/units/dimension.cpp
+++ physics/basic/units/dimension.cpp
@@ -231,9 +231,9 @@ namespace physics::units
 		string dimension_name;
 		string prefix_name;
 		set<string> prefixes_options {"Y", "E", "P", "T", "G", "M", "k", "h", "d", "c", "m", "u", "n", "p", "f", "a", "z", "y"};
-		if(prefixes_options.count(x.substr(0,1)) and x != "angle" and x != "solid_angle" and x != "m")
+		if(prefixes_options.count(x.substr(0,1)) and x != "angle" and x != "solid_angle" and x != "m" and x.substr(0,2) != "da")
 		{
-			prefix_name = x.substr(0, 1);
+			prefix_name = x.substr(0,1);
 			dimension_name = x.substr(1);
 			new_prefix = create_prefix(prefix_name);
 		}
