947a2fb9af0057451c923fa14b7daa62cfa6ed47
Ismael Correa <ismael.correa.castro@gmail.com>
2017-08-08 18:17:49 -0400

    chemistry/constants.hpp file including the avogadro constant for working with particles. It was from a previous commit

---
+++ chemistry/constants.hpp
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_HPP_INCLUDED
+#define CONSTANTS_HPP_INCLUDED
+
+using namespace std;
+
+namespace chemistry
+{
+	const static long long int avogadro_constant = 602214078000000000000000L;
+}
+
+#endif // CONSTANTS_HPP_INCLUDED
