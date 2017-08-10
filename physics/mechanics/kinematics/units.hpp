94e82f9c3b9d2f700bbfd362426fa25448364b1d
Ismael Correa <ismael.correa.castro@gmail.com>
2017-08-10 19:11:14 -0400

    kinematics units now use the new macros to define scalar and vector units

--- physics/mechanics/kinematics/units.hpp
+++ physics/mechanics/kinematics/units.hpp
@@ -9,77 +9,14 @@ using namespace std;
 
 namespace physics::units
 {
-	class distance : public scalar_unit_crtp<distance>
-	{
-		public:
-			using scalar_unit_crtp::scalar_unit_crtp;
-
-			static const string dimensions_match;
-			static const vector_real_dimensions real_dimensions;
-	};
-
-	class velocity : public vector_unit_crtp<velocity>
-	{
-		public:
-			using vector_unit_crtp::vector_unit_crtp;
-
-			static const string dimensions_match;
-			static const vector_real_dimensions real_dimensions;
-	};
-
-	class acceleration : public vector_unit_crtp<acceleration>
-	{
-		public:
-			using vector_unit_crtp::vector_unit_crtp;
-
-			static const string dimensions_match;
-			static const vector_real_dimensions real_dimensions;
-	};
-
-	class jerk : public vector_unit_crtp<jerk>
-	{
-		public:
-			using vector_unit_crtp::vector_unit_crtp;
-
-			static const string dimensions_match;
-			static const vector_real_dimensions real_dimensions;
-	};
-
-	class snap : public vector_unit_crtp<snap>
-	{
-		public:
-			using vector_unit_crtp::vector_unit_crtp;
-
-			static const string dimensions_match;
-			static const vector_real_dimensions real_dimensions;
-	};
-
-	class angular_velocity : public vector_unit_crtp<angular_velocity>
-	{
-		public:
-			using vector_unit_crtp::vector_unit_crtp;
-
-			static const string dimensions_match;
-			static const vector_real_dimensions real_dimensions;
-	};
-
-	class angular_acceleration : public vector_unit_crtp<angular_acceleration>
-	{
-		public:
-			using vector_unit_crtp::vector_unit_crtp;
-
-			static const string dimensions_match;
-			static const vector_real_dimensions real_dimensions;
-	};
-
-	class angular_momentum : public vector_unit_crtp<angular_momentum>
-	{
-		public:
-			using vector_unit_crtp::vector_unit_crtp;
-
-			static const string dimensions_match;
-			static const vector_real_dimensions real_dimensions;
-	};
+	SCALAR_UNIT_HPP(distance);
+	VECTOR_UNIT_HPP(velocity);
+	VECTOR_UNIT_HPP(acceleration);
+	VECTOR_UNIT_HPP(jerk);
+	VECTOR_UNIT_HPP(snap);
+	VECTOR_UNIT_HPP(angular_velocity);
+	VECTOR_UNIT_HPP(angular_acceleration);
+	VECTOR_UNIT_HPP(angular_momentum);
 }
 
 #endif // PHYSICS_MECHANICS_MOVEMENT_UNITS_HPP_INCLUDED
