01d10745155716c4bc498f89a3fcbbb428b594fd
Ismael Correa <ismael.correa.castro@gmail.com>
2017-09-05 17:08:30 -0300

    All atoms classes of group AC created with all their properties

---
+++ chemistry/general/atoms/atoms_ac.cpp
@@ -0,0 +1,214 @@
+#include "atoms_ac.hpp"
+
+namespace chemistry
+{
+	ATOM_CPP(atom_Ac)
+
+	const string atom_Ac::name = "Actinium";
+	const string atom_Ac::symbol = "Ac";
+
+	const length atom_Ac::atomic_radius = 195_pm;
+	const mass atom_Ac::standard_atomic_mass = 227.028_amu;
+
+	const molar_energy atom_Ac::ionization_energy ("499 kJ/mol");
+	const length atom_Ac::covalent_radius = 215_pm;
+	const length atom_Ac::van_der_waals_radius = 0_pm;
+
+	const cas_number atom_Ac::cas_number ("7440-34-8");
+
+	ATOM_CPP(atom_Th)
+
+	const string atom_Th::name = "Thorium";
+	const string atom_Th::symbol = "Th";
+
+	const length atom_Th::atomic_radius = 179.8_pm;
+	const mass atom_Th::standard_atomic_mass = 232.038_amu;
+
+	const molar_energy atom_Th::ionization_energy ("587 kJ/mol");
+	const length atom_Th::covalent_radius = 206_pm;
+	const length atom_Th::van_der_waals_radius = 0_pm;
+
+	const cas_number atom_Th::cas_number ("7440-29-1");
+
+	ATOM_CPP(atom_Pa)
+
+	const string atom_Pa::name = "Protactinium";
+	const string atom_Pa::symbol = "Pa";
+
+	const length atom_Pa::atomic_radius = 163_pm;
+	const mass atom_Pa::standard_atomic_mass = 231.036_amu;
+
+	const molar_energy atom_Pa::ionization_energy ("568 kJ/mol");
+	const length atom_Pa::covalent_radius = 200_pm;
+	const length atom_Pa::van_der_waals_radius = 0_pm;
+
+	const cas_number atom_Pa::cas_number ("7440-13-3");
+
+	ATOM_CPP(atom_U)
+
+	const string atom_U::name = "Uranium";
+	const string atom_U::symbol = "U";
+
+	const length atom_U::atomic_radius = 156_pm;
+	const mass atom_U::standard_atomic_mass = 238.029_amu;
+
+	const molar_energy atom_U::ionization_energy ("597.6 kJ/mol");
+	const length atom_U::covalent_radius = 196_pm;
+	const length atom_U::van_der_waals_radius = 186_pm;
+
+	const cas_number atom_U::cas_number ("7440-61-1");
+
+	ATOM_CPP(atom_Np)
+
+	const string atom_Np::name = "Neptunium";
+	const string atom_Np::symbol = "Np";
+
+	const length atom_Np::atomic_radius = 155_pm;
+	const mass atom_Np::standard_atomic_mass = 237.048_amu;
+
+	const molar_energy atom_Np::ionization_energy ("604.5 kJ/mol");
+	const length atom_Np::covalent_radius = 190_pm;
+	const length atom_Np::van_der_waals_radius = 0_pm;
+
+	const cas_number atom_Np::cas_number ("7439-99-8");
+
+	ATOM_CPP(atom_Pu)
+
+	const string atom_Pu::name = "Plutonium";
+	const string atom_Pu::symbol = "Pu";
+
+	const length atom_Pu::atomic_radius = 159_pm;
+	const mass atom_Pu::standard_atomic_mass = 244.064_amu;
+
+	const molar_energy atom_Pu::ionization_energy ("584.7 kJ/mol");
+	const length atom_Pu::covalent_radius = 187_pm;
+	const length atom_Pu::van_der_waals_radius = 0_pm;
+
+	const cas_number atom_Pu::cas_number ("7440-07-5");
+
+	ATOM_CPP(atom_Am)
+
+	const string atom_Am::name = "Americium";
+	const string atom_Am::symbol = "Am";
+
+	const length atom_Am::atomic_radius = 173_pm;
+	const mass atom_Am::standard_atomic_mass = 243.061_amu;
+
+	const molar_energy atom_Am::ionization_energy ("578 kJ/mol");
+	const length atom_Am::covalent_radius = 180_pm;
+	const length atom_Am::van_der_waals_radius = 0_pm;
+
+	const cas_number atom_Am::cas_number ("7440-35-9");
+
+	ATOM_CPP(atom_Cm)
+
+	const string atom_Cm::name = "Curium";
+	const string atom_Cm::symbol = "Cm";
+
+	const length atom_Cm::atomic_radius = 174_pm;
+	const mass atom_Cm::standard_atomic_mass = 247.070_amu;
+
+	const molar_energy atom_Cm::ionization_energy ("581 kJ/mol");
+	const length atom_Cm::covalent_radius = 169_pm;
+	const length atom_Cm::van_der_waals_radius = 0_pm;
+
+	const cas_number atom_Cm::cas_number ("7440-51-9");
+
+	ATOM_CPP(atom_Bk)
+
+	const string atom_Bk::name = "Berkelium";
+	const string atom_Bk::symbol = "Bk";
+
+	const length atom_Bk::atomic_radius = 170_pm;
+	const mass atom_Bk::standard_atomic_mass = 247.070_amu;
+
+	const molar_energy atom_Bk::ionization_energy ("601 kJ/mol");
+	const length atom_Bk::covalent_radius = 0_pm;
+	const length atom_Bk::van_der_waals_radius = 0_pm;
+
+	const cas_number atom_Bk::cas_number ("7440-40-6");
+
+	ATOM_CPP(atom_Cf)
+
+	const string atom_Cf::name = "Californium";
+	const string atom_Cf::symbol = "Cf";
+
+	const length atom_Cf::atomic_radius = 0_pm;
+	const mass atom_Cf::standard_atomic_mass = 251.080_amu;
+
+	const molar_energy atom_Cf::ionization_energy ("608 kJ/mol");
+	const length atom_Cf::covalent_radius = 0_pm;
+	const length atom_Cf::van_der_waals_radius = 0_pm;
+
+	const cas_number atom_Cf::cas_number ("7440-71-3");
+
+	ATOM_CPP(atom_Es)
+
+	const string atom_Es::name = "Einsteinium";
+	const string atom_Es::symbol = "Es";
+
+	const length atom_Es::atomic_radius = 0_pm;
+	const mass atom_Es::standard_atomic_mass = 254_amu;
+
+	const molar_energy atom_Es::ionization_energy ("619 kJ/mol");
+	const length atom_Es::covalent_radius = 0_pm;
+	const length atom_Es::van_der_waals_radius = 0_pm;
+
+	const cas_number atom_Es::cas_number ("7429-92-7");
+
+	ATOM_CPP(atom_Fm)
+
+	const string atom_Fm::name = "Fermium";
+	const string atom_Fm::symbol = "Fm";
+
+	const length atom_Fm::atomic_radius = 0_pm;
+	const mass atom_Fm::standard_atomic_mass = 257.095_amu;
+
+	const molar_energy atom_Fm::ionization_energy ("627 kJ/mol");
+	const length atom_Fm::covalent_radius = 0_pm;
+	const length atom_Fm::van_der_waals_radius = 0_pm;
+
+	const cas_number atom_Fm::cas_number ("7440-72-4");
+
+	ATOM_CPP(atom_Md)
+
+	const string atom_Md::name = "Mendelevium";
+	const string atom_Md::symbol = "Md";
+
+	const length atom_Md::atomic_radius = 0_pm;
+	const mass atom_Md::standard_atomic_mass = 258.1_amu;
+
+	const molar_energy atom_Md::ionization_energy ("635 kJ/mol");
+	const length atom_Md::covalent_radius = 0_pm;
+	const length atom_Md::van_der_waals_radius = 0_pm;
+
+	const cas_number atom_Md::cas_number ("7440-11-1");
+
+	ATOM_CPP(atom_No)
+
+	const string atom_No::name = "Nobelium";
+	const string atom_No::symbol = "No";
+
+	const length atom_No::atomic_radius = 0_pm;
+	const mass atom_No::standard_atomic_mass = 259.101_amu;
+
+	const molar_energy atom_No::ionization_energy ("641.6 kJ/mol");
+	const length atom_No::covalent_radius = 0_pm;
+	const length atom_No::van_der_waals_radius = 0_pm;
+
+	const cas_number atom_No::cas_number ("10028-14-5");
+
+	ATOM_CPP(atom_Lr)
+
+	const string atom_Lr::name = "Lawrencium";
+	const string atom_Lr::symbol = "Lr";
+
+	const length atom_Lr::atomic_radius = 0_pm;
+	const mass atom_Lr::standard_atomic_mass = 262_amu;
+
+	const molar_energy atom_Lr::ionization_energy ("478.6 kJ/mol");
+	const length atom_Lr::covalent_radius = 0_pm;
+	const length atom_Lr::van_der_waals_radius = 0_pm;
+
+	const cas_number atom_Lr::cas_number ("22537-19-5");
+}
---
+++ chemistry/general/atoms/atoms_ac.hpp
@@ -0,0 +1,461 @@
+#ifndef CHEMISTRY_GENERAL_ATOMS_ATOMS_AC_HPP
+#define CHEMISTRY_GENERAL_ATOMS_ATOMS_AC_HPP
+
+#include "normal_atom.hpp"
+
+#include "units.hpp"
+
+namespace chemistry
+{
+	class atom_Ac : public normal_atom<atom_Ac>
+	{
+		public:
+			ATOM_HPP(atom_Ac);
+
+			static const atom_symbol enum_type = atom_symbol::Ac;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::d;
+
+			static const int z = 89;
+			static const int mass_number = 138;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.1;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+
+	class atom_Th : public normal_atom<atom_Th>
+	{
+		public:
+			ATOM_HPP(atom_Th);
+
+			static const atom_symbol enum_type = atom_symbol::Th;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::f;
+
+			static const int z = 90;
+			static const int mass_number = 142;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.3;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+
+	class atom_Pa : public normal_atom<atom_Pa>
+	{
+		public:
+			ATOM_HPP(atom_Pa);
+
+			static const atom_symbol enum_type = atom_symbol::Pa;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::f;
+
+			static const int z = 91;
+			static const int mass_number = 140;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.5;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+
+	class atom_U : public normal_atom<atom_U>
+	{
+		public:
+			ATOM_HPP(atom_U);
+
+			static const atom_symbol enum_type = atom_symbol::U;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::f;
+
+			static const int z = 92;
+			static const int mass_number = 146;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.38;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+
+	class atom_Np : public normal_atom<atom_Np>
+	{
+		public:
+			ATOM_HPP(atom_Np);
+
+			static const atom_symbol enum_type = atom_symbol::Np;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::f;
+
+			static const int z = 93;
+			static const int mass_number = 144;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.36;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+
+	class atom_Pu : public normal_atom<atom_Pu>
+	{
+		public:
+			ATOM_HPP(atom_Pu);
+
+			static const atom_symbol enum_type = atom_symbol::Pu;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::f;
+
+			static const int z = 94;
+			static const int mass_number = 150;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.28;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+
+	class atom_Am : public normal_atom<atom_Am>
+	{
+		public:
+			ATOM_HPP(atom_Am);
+
+			static const atom_symbol enum_type = atom_symbol::Am;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::f;
+
+			static const int z = 95;
+			static const int mass_number = 248;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.3;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+
+	class atom_Cm : public normal_atom<atom_Cm>
+	{
+		public:
+			ATOM_HPP(atom_Cm);
+
+			static const atom_symbol enum_type = atom_symbol::Cm;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::f;
+
+			static const int z = 96;
+			static const int mass_number = 151;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.3;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+
+	class atom_Bk : public normal_atom<atom_Bk>
+	{
+		public:
+			ATOM_HPP(atom_Bk);
+
+			static const atom_symbol enum_type = atom_symbol::Bk;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::f;
+
+			static const int z = 97;
+			static const int mass_number = 150;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.3;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+
+	class atom_Cf : public normal_atom<atom_Cf>
+	{
+		public:
+			ATOM_HPP(atom_Cf);
+
+			static const atom_symbol enum_type = atom_symbol::Cf;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::f;
+
+			static const int z = 98;
+			static const int mass_number = 153;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.3;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+
+	class atom_Es : public normal_atom<atom_Es>
+	{
+		public:
+			ATOM_HPP(atom_Es);
+
+			static const atom_symbol enum_type = atom_symbol::Es;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::f;
+
+			static const int z = 99;
+			static const int mass_number = 155;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.3;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+
+	class atom_Fm : public normal_atom<atom_Fm>
+	{
+		public:
+			ATOM_HPP(atom_Fm);
+
+			static const atom_symbol enum_type = atom_symbol::Fm;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::f;
+
+			static const int z = 100;
+			static const int mass_number = 157;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.3;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+
+	class atom_Md : public normal_atom<atom_Md>
+	{
+		public:
+			ATOM_HPP(atom_Md);
+
+			static const atom_symbol enum_type = atom_symbol::Md;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::f;
+
+			static const int z = 101;
+			static const int mass_number = 157;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.3;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+
+	class atom_No : public normal_atom<atom_No>
+	{
+		public:
+			ATOM_HPP(atom_No);
+
+			static const atom_symbol enum_type = atom_symbol::No;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::f;
+
+			static const int z = 102;
+			static const int mass_number = 157;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.3;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+
+	class atom_Lr : public normal_atom<atom_Lr>
+	{
+		public:
+			ATOM_HPP(atom_Lr);
+
+			static const atom_symbol enum_type = atom_symbol::Lr;
+
+			static const string name;
+			static const string symbol;
+
+			static const atomic_group group = AC;
+			static const int period = 7;
+			static const atomic_block block = atomic_block::f;
+
+			static const int z = 103;
+			static const int mass_number = 159;
+
+			static const length atomic_radius;
+			static const mass standard_atomic_mass;
+
+			static constexpr float electronegativity = 1.3;
+			static const bool radioactive = false;
+
+			static const molar_energy ionization_energy;
+			static const length covalent_radius;
+			static const length van_der_waals_radius;
+
+			static const cas_number cas_number;
+	};
+}
+
+#endif // CHEMISTRY_GENERAL_ATOMS_ATOMS_AC_HPP
