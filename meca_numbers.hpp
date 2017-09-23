0081ac5213f9ca0988d2e163fab89db427191536
Ismael Correa <ismael.correa.castro@gmail.com>
2017-09-23 11:38:47 -0300

    headers for math classes added

---
+++ meca_numbers.hpp
@@ -0,0 +1,11 @@
+#ifndef MECA_NUMBERS_HPP_INCLUDED
+#define MECA_NUMBERS_HPP_INCLUDED
+
+#include "math/linear_algebra/matrix.hpp"
+
+#include "math/number/undefined_number.hpp"
+#include "math/number/unit_number.hpp"
+#include "math/number/angle_number.hpp"
+#include "math/number/lab_number.hpp"
+
+#endif // MECA_NUMBERS_HPP_INCLUDED
