1731c908d59da84121ab03b850073edb22e3fe39
Ismael Correa <ismael.correa.castro@gmail.com>
2017-09-22 22:08:41 -0300

    class bit_molecule created

--- chemistry/general/molecules/bit_molecule.hpp
+++ chemistry/general/molecules/bit_molecule.hpp
@@ -1,6 +1,17 @@
 #ifndef BIT_MOLECULE_HPP_INCLUDED
 #define BIT_MOLECULE_HPP_INCLUDED
 
+#include "molecule.hpp"
+
 using namespace std;
+
+namespace chemistry
+{
+	class bit_molecule : public molecule
+	{
+		public:
+			bit_molecule();
+	};
+}
 
 #endif // BIT_MOLECULE_HPP_INCLUDED
