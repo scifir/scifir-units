1c625e2d8ddd677b57e1a1c47475d541687d1bad
Ismael Correa <ismael.correa.castro@gmail.com>
2023-06-23 22:01:28 -0400

    header guard of meca_numbers.hpp improved

--- meca_numbers.hpp
+++ meca_numbers.hpp
@@ -1,9 +1,9 @@
-#ifndef MECA_NUMBERS_HPP_INCLUDED
-#define MECA_NUMBERS_HPP_INCLUDED
+#ifndef MSCI_UNITS_MECA_NUMBERS_HPP_INCLUDED
+#define MSCI_UNITS_MECA_NUMBERS_HPP_INCLUDED
 
 #include "util/matrix.hpp"
 
 #include "meca_number/angle.hpp"
 #include "meca_number/lab_number.hpp"
 
-#endif // MECA_NUMBERS_HPP_INCLUDED
+#endif // MSCI_UNITS_MECA_NUMBERS_HPP_INCLUDED
