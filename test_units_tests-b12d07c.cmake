b0da8225e419eaf1b5ba3d18442b41c0252cf2c0
Ismael Correa <ismael.correa.castro@gmail.com>
2023-06-25 18:24:39 -0400

    error_log and generated cmake file removed from root folder to improve order

--- test_units_tests-b12d07c.cmake
+++
@@ -1,3 +0,0 @@
-add_test( [==[scalar_unit class]==] /home/iarfen/Proyectos/Cientificos/Laboratorio/msci/units/test_units [==[scalar_unit class]==]  )
-set_tests_properties( [==[scalar_unit class]==] PROPERTIES WORKING_DIRECTORY /home/iarfen/Proyectos/Cientificos/Laboratorio/msci/units)
-set( test_units_TESTS [==[scalar_unit class]==])
