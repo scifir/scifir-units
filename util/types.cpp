851038ab406ae54794305975bb6a8b23b0b7f7aa
Ismael Correa <ismael.correa.castro@gmail.com>
2023-06-26 19:31:40 -0400

    display of all decimals for all numeric displays added

--- util/types.cpp
+++ util/types.cpp
@@ -36,13 +36,20 @@ namespace msci
 	string display_float(const float& value,int number_of_decimals)
 	{
 		ostringstream output;
-		float fract_value,int_value;
-		fract_value = modf(value,&int_value);
-		output << (int)int_value;
-		if (fract_value != 0)
+		if (number_of_decimals > 0)
 		{
-			output << ".";
-			output << int(fract_value*std::pow(10,number_of_decimals));
+			float fract_value,int_value;
+			fract_value = modf(value,&int_value);
+			output << (int)int_value;
+			if (fract_value != 0)
+			{
+				output << ".";
+				output << int(fract_value*std::pow(10,number_of_decimals));
+			}
+		}
+		else
+		{
+			output << value;
 		}
 		return output.str();
 	}
