d474bf68a98517d9b6cd487ddddaa9fe0241b2d6
Ismael Correa Castro <ismael.correa.castro@gmail.com>
2024-06-26 15:16:14 -0400

    test of point_1d class finished with 100% coverage

---
+++ tests/topology/test_point_1d.cpp
@@ -0,0 +1,224 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch2/catch_all.hpp"
+#include "catch2/catch_test_macros.hpp"
+#include "../../topology/point_1d.hpp"
+#include "../../topology/point_1d_impl.hpp"
+#include "../../coordinates/coordinates_1d.hpp"
+#include "../../units/unit_basic.hpp"
+
+#include <string>
+#include <sstream>
+
+using namespace std;
+using namespace scifir;
+
+TEST_CASE("class point_1d","Complete test of point_1d")
+{
+	SECTION("Constructors of point_1d<> classes")
+	{
+		point_1d<> a;
+		REQUIRE (bool(to_string(a) == "(0 m)"));
+		REQUIRE (bool(to_string(a.x) == "0 m"));
+		point_1d<> a2(5_m);
+		point_1d<> a3(a2);
+		REQUIRE (bool(to_string(a3) == "(5 m)"));
+		point_1d<> a4(5_m);
+		point_1d<> a5(std::move(a4));
+		REQUIRE (bool(to_string(a3) == "(5 m)"));
+		point_1d<> b(100_m);
+		REQUIRE (bool(to_string(b) == "(100 m)"));
+		coordinates_1d<> c2(100_m);
+		point_1d<> c(c2);
+		REQUIRE (bool(to_string(c) == "(100 m)"));
+		point_1d<> d("(1 m)");
+		REQUIRE (bool(to_string(d) == "(1 m)"));
+		point_1d<> e("1 m");
+		REQUIRE (bool(to_string(e) == "(1 m)"));
+	}
+
+	SECTION("Constructors of point_1d<float> classes")
+	{
+		point_1d<float> a;
+		REQUIRE (bool(to_string(a) == "(0)"));
+		REQUIRE (bool(a.x == 0.0f));
+		point_1d<float> a2(5.0f);
+		point_1d<float> a3(a2);
+		REQUIRE (bool(to_string(a3) == "(5)"));
+		point_1d<float> a4(5.0f);
+		point_1d<float> a5(std::move(a4));
+		REQUIRE (bool(to_string(a3) == "(5)"));
+		point_1d<float> b(100.0f);
+		REQUIRE (bool(to_string(b) == "(100)"));
+		coordinates_1d<float> c2(100.0f);
+		point_1d<float> c(c2);
+		REQUIRE (bool(to_string(c) == "(100)"));
+		point_1d<float> d("(1)");
+		REQUIRE (bool(to_string(d) == "(1)"));
+		point_1d<float> e("1");
+		REQUIRE (bool(to_string(e) == "(1)"));
+	}
+
+	SECTION("Assignments of point_1d<> classes")
+	{
+		point_1d<> a(10_m);
+		point_1d<> b(20_m);
+		a = b;
+		REQUIRE (bool(to_string(a) == "(20 m)"));
+		point_1d<> c(10_m);
+		point_1d<> d(30_m);
+		c = std::move(d);
+		REQUIRE (bool(to_string(c) == "(30 m)"));
+		point_1d<> e(10_m);
+		coordinates_1d<> f(15_m);
+		e = f;
+		REQUIRE (bool(to_string(e) == "(15 m)"));
+	}
+
+	SECTION("Assignments of point_1d<float> classes")
+	{
+		point_1d<float> a(10.0f);
+		point_1d<float> b(20.0f);
+		a = b;
+		REQUIRE (bool(to_string(a) == "(20)"));
+		point_1d<float> c(10.0f);
+		point_1d<float> d(30.0f);
+		c = std::move(d);
+		REQUIRE (bool(to_string(c) == "(30)"));
+		point_1d<float> e(10.0f);
+		coordinates_1d<float> f(15.0f);
+		e = f;
+		REQUIRE (bool(to_string(e) == "(15)"));
+	}
+
+	SECTION("move(), set_position() and distance_to_origin() of point_1d<> classes")
+	{
+		point_1d<> a(5_m);
+		a.set_position(25_m);
+		REQUIRE (bool(to_string(a) == "(25 m)"));
+		point_1d<> b(10_m);
+		b.move(35_m);
+		REQUIRE (bool(to_string(b) == "(45 m)"));
+		point_1d<> c(10_m);
+		length d = c.distance_to_origin();
+		REQUIRE (bool(to_string(d) == "10 m"));
+	}
+
+	SECTION("move(), set_position() and distance_to_origin() of point_1d<float> classes")
+	{
+		point_1d<float> a(5.0f);
+		a.set_position(25.0f);
+		REQUIRE (bool(to_string(a) == "(25)"));
+		point_1d<float> b(10.0f);
+		b.move(35.0f);
+		REQUIRE (bool(to_string(b) == "(45)"));
+		point_1d<float> c(10.0f);
+		REQUIRE (bool(c.distance_to_origin() == 10.0f));
+	}
+
+	SECTION("distance() functions of point_1d<> classes")
+	{
+		point_1d<> a(10_m);
+		point_1d<> b(15_m);
+		REQUIRE (bool(to_string(distance(a,b)) == "5 m"));
+		point_1d<> c(20_m);
+		coordinates_1d<> d(35_m);
+		REQUIRE (bool(to_string(distance(c,d)) == "15 m"));
+		REQUIRE (bool(to_string(distance(d,c)) == "15 m"));
+	}
+
+	SECTION("distance() functions of point_1d<float> classes")
+	{
+		point_1d<float> a(10.0f);
+		point_1d<float> b(15.0f);
+		REQUIRE (bool(distance(a,b) == 5.0f));
+		point_1d<float> c(20.0f);
+		coordinates_1d<float> d(35.0f);
+		REQUIRE (bool(distance(c,d) == 15.0f));
+		REQUIRE (bool(distance(d,c) == 15.0f));
+	}
+
+	SECTION("Relational operators of point_1d<> classes")
+	{
+		point_1d<> a(7_m);
+		point_1d<> b(7_m);
+		point_1d<> c(6_m);
+		REQUIRE(bool(a == b));
+		REQUIRE(bool((a == c)) == false);
+		REQUIRE(bool(a != c));
+		coordinates_1d<> d(7_m);
+		coordinates_1d<> e(5_m);
+		REQUIRE(bool(a == d));
+		REQUIRE(bool(a != e));
+		REQUIRE(bool(d == a));
+		REQUIRE(bool(e != a));
+	}
+
+	SECTION("Relational operators of point_1d<float> classes")
+	{
+		point_1d<float> a(7.0f);
+		point_1d<float> b(7.0f);
+		point_1d<float> c(6.0f);
+		REQUIRE(bool(a == b));
+		REQUIRE(bool((a == c)) == false);
+		REQUIRE(bool(a != c));
+		coordinates_1d<float> d(7.0f);
+		coordinates_1d<float> e(5.0f);
+		REQUIRE(bool(a == d));
+		REQUIRE(bool(a != e));
+		REQUIRE(bool(d == a));
+		REQUIRE(bool(e != a));
+	}
+
+	SECTION("String operators of point_1d<> classes")
+	{
+		point_1d<> a(7_m);
+		REQUIRE(bool(a == "(7 m)"));
+		REQUIRE(bool(a != "(6 m)"));
+		REQUIRE(bool("(7 m)" == a));
+		REQUIRE(bool("(6 m)" != a));
+		std::string b = "x: ";
+		b += a;
+		REQUIRE(bool(b == "x: (7 m)"));
+		REQUIRE(bool(("x: " + a) == "x: (7 m)"));
+		REQUIRE(bool("x: (7 m)" == ("x: " + a)));
+	}
+
+	SECTION("String operators of point_1d<float> classes")
+	{
+		point_1d<float> a(7.0f);
+		REQUIRE(bool(a == "(7)"));
+		REQUIRE(bool(a != "(6)"));
+		REQUIRE(bool("(7)" == a));
+		REQUIRE(bool("(6)" != a));
+		std::string b = "x: ";
+		b += a;
+		REQUIRE(bool(b == "x: (7)"));
+		REQUIRE(bool(("x: " + a) == "x: (7)"));
+		REQUIRE(bool("x: (7)" == ("x: " + a)));
+	}
+
+	SECTION("Streams of point_1d<> classes")
+	{
+		point_1d<> a(2_m);
+		std::ostringstream out;
+		out << a;
+		REQUIRE(bool(out.str() == "(2 m)"));
+		std::istringstream iss("(1 m)");
+		point_1d<> b;
+		iss >> b;
+		REQUIRE(bool(to_string(b) == "(1 m)"));
+	}
+
+	SECTION("Streams of point_1d<float> classes")
+	{
+		point_1d<float> a(2.0f);
+		std::ostringstream out;
+		out << a;
+		REQUIRE(bool(out.str() == "(2)"));
+		std::istringstream iss("(1)");
+		point_1d<float> b;
+		iss >> b;
+		REQUIRE(bool(to_string(b) == "(1)"));
+	}
+}
