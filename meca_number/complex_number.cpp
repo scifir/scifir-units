b6ec3f9634d35d54b108ac529d2b57a04d285eb4
Ismael Correa Castro <ismael.correa.castro@gmail.com>
2024-07-03 18:53:49 -0400

    complex_number<> class updated with some fixes

--- meca_number/complex_number.cpp
+++ meca_number/complex_number.cpp
@@ -6,18 +6,20 @@ namespace scifir
 {
 	bool is_complex(const string& init_complex_number)
 	{
-		if (init_complex_number.find("+"))
+		if ((init_complex_number.find("+") != string::npos or init_complex_number.find("-") != string::npos) and init_complex_number.length() > 1)
 		{
 			vector<string> numbers;
-			boost::split(numbers,init_complex_number,boost::is_any_of("+"));
+			boost::split(numbers,init_complex_number,boost::is_any_of("+-"));
 			if (numbers.size() != 2)
 			{
 				return false;
 			}
 			int imaginary_length = int(numbers[1].length()) - 3;
-			if (numbers[1].substr(0,imaginary_length) == "(i)")
+			if (numbers[1].substr(imaginary_length) == "(i)")
 			{
 				bool dot_present = false;
+				bool after_whitespace = false;
+				bool after_alpha = false;
 				for (unsigned int i = 0; i < numbers[0].length(); i++)
 				{
 					if (numbers[0][i] == '.')
@@ -29,15 +31,38 @@ namespace scifir
 						else
 						{
 							dot_present = true;
+							continue;
 						}
 					}
-					else if (!std::isdigit(numbers[0][i]))
+					else if (numbers[0][i] == ' ')
+					{
+						after_whitespace = true;
+						continue;
+					}
+					else if (!std::isdigit(numbers[0][i]) and after_whitespace == false)
 					{
 						return false;
 					}
+					else if (!std::isalpha(numbers[0][i]) and after_whitespace == true)
+					{
+						if (after_alpha == true and std::isalpha(numbers[0][i]))
+						{
+							return false;
+						}
+						else if (std::isdigit(numbers[0][i]))
+						{
+							after_alpha = true;
+						}
+						else
+						{
+							return false;
+						}
+					}
 				}
 				dot_present = false;
-				for (int i = 0; i < (imaginary_length); i++)
+				after_whitespace = false;
+				after_alpha = false;
+				for (unsigned int i = 0; i < imaginary_length; i++)
 				{
 					if (numbers[1][i] == '.')
 					{
@@ -48,11 +73,32 @@ namespace scifir
 						else
 						{
 							dot_present = true;
+							continue;
 						}
 					}
-					else if (!std::isdigit(numbers[1][i]))
+					else if (numbers[1][i] == ' ')
+					{
+						after_whitespace = true;
+						continue;
+					}
+					else if (!std::isdigit(numbers[1][i]) and after_whitespace == false)
 					{
 						return false;
+					}
+					else if (!std::isalpha(numbers[1][i]) and after_whitespace == true)
+					{
+						if (after_alpha == true and std::isalpha(numbers[1][i]))
+						{
+							return false;
+						}
+						else if (std::isdigit(numbers[1][i]))
+						{
+							after_alpha = true;
+						}
+						else
+						{
+							return false;
+						}
 					}
 				}
 				return true;
