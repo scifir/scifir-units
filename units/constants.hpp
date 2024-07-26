25af2d6bfdaf6d158ca5d916b21af1ba1bad1b9a
Ismael Correa Castro <ismael.correa.castro@gmail.com>
2024-07-26 16:07:36 -0400

    File constants.hpp updated with all SI constants added as long double

--- units/constants.hpp
+++ units/constants.hpp
@@ -1,5 +1,5 @@
-#ifndef SCIFIR_UNITS_TOPOLOGY_CONSTANTS_HPP_INCLUDED
-#define SCIFIR_UNITS_TOPOLOGY_CONSTANTS_HPP_INCLUDED
+#ifndef SCIFIR_UNITS_UNITS_CONSTANTS_HPP_INCLUDED
+#define SCIFIR_UNITS_UNITS_CONSTANTS_HPP_INCLUDED
 
 #include "boost/math/constants/constants.hpp"
 
@@ -8,7 +8,13 @@ using namespace std;
 namespace scifir
 {
 	constexpr float PI = boost::math::constants::pi<float>();
-	const static long double AVOGADRO_CONSTANT = 6.02214076e23;
+	const long double HYPERFINE_TRANSITION_FREQUENCY_OF_CS = 9192631770.0l;
+	const long double SPEED_OF_LIGHT = 299792458.0l;
+	const long double PLANCK_CONSTANT = 6.62607015e-34;
+	const long double ELEMENTARY_CHARGE = 1.602176634e-19;
+	const long double BOLTZMANN_CONSTANT = 1.380649e-23;
+	const long double AVOGADRO_CONSTANT = 6.02214076e23;
+	const long double LUMINOUS_EFFICACY_OF_540_THZ_RADIATION = 683.0l;
 }
 
-#endif // SCIFIR_UNITS_TOPOLOGY_CONSTANTS_HPP_INCLUDED
+#endif // SCIFIR_UNITS_UNITS_CONSTANTS_HPP_INCLUDED
