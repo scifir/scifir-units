0614b76718cc3fcb776947bd5b201b1e11b91bbc
Ismael Correa Castro <ismael.correa.castro@gmail.com>
2024-08-03 15:19:59 -0400

    Tests of mind.hpp created. Enum classes of mind.hpp updated with new values, and all to_string() functions finished. Test of mind.hpp added to CMakeLists.txt

--- special_units/mind.hpp
+++ special_units/mind.hpp
@@ -15,11 +15,16 @@ namespace scifir
 
     enum class sensation { LIGHT, SOUND, ACCELERATION, ODOR, TASTE, TOUCH, TEMPERATURE };
 
+    enum class sense { SIGHT, HEARING, SMELL, TOUCH, TASTE, THERMOCEPTION, PROPRIOCEPTION };
+
     enum class conscience_state { NORMAL, ALTERED };
 
+    string to_string(const alert_state&);
     string to_string(const feeling&);
     string to_string(const emotion&);
     string to_string(const sensation&);
+    string to_string(const sense&);
+    string to_string(const conscience_state&);
 }
 
 #endif // SCIFIR_UNITS_SPECIAL_UNITS_MIND_HPP_INCLUDED
