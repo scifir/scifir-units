8decf426a7ac1172a09e40090eabca209fbceecb
Ismael Correa Castro <ismael.correa.castro@gmail.com>
2025-02-10 23:16:22 -0300

    Constants of WGS84 updated to be instantiated by a cpp file instead

---
+++ units/constants.cpp
@@ -0,0 +1,9 @@
+#include "./constants.hpp"
+
+using namespace std;
+
+namespace scifir
+{
+	const scalar_unit WGS84_EARTH_SEMIAXIS_A = 6378137_m;
+	const scalar_unit WGS84_EARTH_SEMIAXIS_B = 6356752.314245_m;
+}
--- units/constants.hpp
+++ units/constants.hpp
@@ -24,8 +24,8 @@ namespace scifir
 	const long double RYDBERG_CONSTANT = 1.0973731568539e7;
 	const long double FARADAY_CONSTANT = 9.64853321233100184e4;
 
-	const scalar_unit WGS84_EARTH_SEMIAXIS_A = 6378137_m;
-	const scalar_unit WGS84_EARTH_SEMIAXIS_B = 6356752.314245_m;
+	extern const scalar_unit WGS84_EARTH_SEMIAXIS_A;
+	extern const scalar_unit WGS84_EARTH_SEMIAXIS_B;
 }
 
 #endif // SCIFIR_UNITS_UNITS_CONSTANTS_HPP_INCLUDED
